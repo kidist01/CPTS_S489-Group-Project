61ae121f99a63098cf44bd8ff6e6f54229911bac
Kidist Gustafsson <100043727+kidist01@users.noreply.github.com>
2025-03-23 01:22:47 -0700

    Inital Database setup

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # CPTS_S489-Group-Project
+What you need installed: 
+-npm 
+-sqlite3 
+-nodemon 
+-sequelize 
+
+Start it:
+-if you see the node_module skip doing npm i 
+instead just do: 
+nodemon 
+
+-if you do npm install 
+npm i
+npm i nodemon 
+nodemon 
---
+++ db.js
@@ -0,0 +1,13 @@
+//using sequlize 
+const {Sequelize} = require('sequelize')
+
+//configure it to the db
+const sequelize = new Sequelize(
+    {
+        //dealing with sqlite do cmsdb1 crearte new cmsdb 
+        dialect: 'sqlite',
+        storage: './database/donations.sqlite'
+    }
+)
+
+module.exports = sequelize
