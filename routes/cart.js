3ee72f0e8944b3662d3a355740d3fae9e63aca08
Tuff Henson <TuffJHenson@gmail.com>
2025-04-11 10:52:54 -0700

    Commit to get back into main to make sure I'm up to date

--- routes/cart.js
+++ routes/cart.js
@@ -1,11 +1,32 @@
 var express = require('express');
 var router = express.Router();
 const User = require('../models/User.js');
+const Cart = require('../models/Cart.js');
+const session = require('express-session');
 
 /* GET home page. */
-router.get('/', (req, res) => {
-  res.render('cart');
+router.get('/', async (req, res) => {
+    let cartItems;
+    let cartSize;
+    let products;
+    console.log(session.user)
+    if(session.user){
+      cartItems = await Cart.findAll({
+        where:{
+          userId: session.user.userId
+        },
+        include: ['Products']
+      })
+    }
+    else{
+      res.redirect("/")
+    }
+
+  cartSize = cartItems ? cartItems.length : 0;
+  res.render('cart', {cartSize, cartItems});
 });
+
+
 
 
 module.exports = router;
